1512101aa4c9609fc81669ebb79786a88377e076
jiapengcai <635793@gmail.com>
2020-05-09 15:40:36 +0800

    [chapter1]exercise14

--- src/main/java/com/jiapengcai/lab/algorithms/chapter1/one/Exercise14.java
+++ src/main/java/com/jiapengcai/lab/algorithms/chapter1/one/Exercise14.java
@@ -1,5 +1,7 @@
 package com.jiapengcai.lab.algorithms.chapter1.one;
 
+import edu.princeton.cs.algs4.StdOut;
+
 /**
  * @author caijiapeng
  * @version created on 2020/5/9 14:22
@@ -7,6 +9,17 @@ package com.jiapengcai.lab.algorithms.chapter1.one;
 public class Exercise14 {
 
     public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        StdOut.println(lg(n));
+    }
 
+    private static int lg(int n) {
+        int count = 0;
+        int m = n / 2;
+        while (m > 0) {
+            count++;
+            m = m / 2;
+        }
+        return count;
     }
 }
